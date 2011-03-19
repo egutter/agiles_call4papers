037840a985df59c85c06f63813f8960a305851b9
Emilio Gutter <egutter@gmail.com>
2011-03-19 14:37:49 -0300

    Fixed migration to be compatible with PostgreSQL

--- db/migrate/20100127223701_change_sessions_audience_limit.rb
+++ db/migrate/20100127223701_change_sessions_audience_limit.rb
@@ -1,10 +1,14 @@
 class ChangeSessionsAudienceLimit < ActiveRecord::Migration
   def self.up
-    change_column :sessions, :audience_limit, :integer
-    Session.update_all('audience_limit = NULL')
+    # Doesn't work in PostgreSQL change_column :sessions, :audience_limit, :integer
+    #Session.update_all('audience_limit = NULL')
+    remove_column :sessions, :audience_limit
+    add_column :sessions, :audience_limit, :integer
   end
 
   def self.down
-    change_column :sessions, :audience_limit, :string
+    # Doesn't work in PostgreSQL 'change_column :sessions, :audience_limit, :string
+    remove_column :sessions, :audience_limit
+    add_column :sessions, :audience_limit, :string
   end
 end

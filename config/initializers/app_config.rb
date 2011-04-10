f76b5b79893f5237b15742513334c891500420aa
Emilio Gutter <egutter@gmail.com>
2011-04-10 14:01:44 -0300

    Comente config de mail para que ande en Heroku

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,5 +1,6 @@
 config = YAML.load_file("#{RAILS_ROOT}/config/config.yml")
 
 ::AppConfig = config
-ActionMailer::Base.smtp_settings = config[:smtp_settings]
+# Commented out so Heroku Sendgrid plugin takes controll of mail config
+#ActionMailer::Base.smtp_settings = config[:smtp_settings]
 ActionMailer::Base.default_url_options[:host] = config[:host]

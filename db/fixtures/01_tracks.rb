0b0bd346314157f0144edfae9dbab273baaf9984
malaimo <malaimo@gmail.com>
2011-04-10 15:12:09 -0300

    New Tracks and basic information

--- db/fixtures/01_tracks.rb
+++ db/fixtures/01_tracks.rb
@@ -6,12 +6,31 @@ end
 
 Track.seed do |track|
   track.id = 2
+  track.title = 'tracks.coaching.title'
+  track.description = 'tracks.coaching.description'
+end
+
+Track.seed do |track|
+  track.id = 3
   track.title = 'tracks.management.title'
   track.description = 'tracks.management.description'
 end
 
 Track.seed do |track|
-  track.id = 3
+  track.id = 4
+  track.title = 'tracks.transformation.title'
+  track.description = 'tracks.transformation.description'
+end
+
+
+Track.seed do |track|
+  track.id = 5
   track.title = 'tracks.engineering.title'
   track.description = 'tracks.engineering.description'
+end
+
+Track.seed do |track|
+  track.id = 6
+  track.title = 'tracks.qa_testing.title'
+  track.description = 'tracks.qa_testing.description'
 end

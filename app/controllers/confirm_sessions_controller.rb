b0b432cf82a486673b90b2bf109c4fbad1b362ef
Emilio Gutter <egutter@10pines.com>
2011-07-12 08:28:09 -0300

    fixed suthorization validation for confirm papers

--- app/controllers/confirm_sessions_controller.rb
+++ app/controllers/confirm_sessions_controller.rb
@@ -16,6 +16,6 @@ class ConfirmSessionsController < ApplicationController
   protected
   def authorize_action
     @session = Session.find(params[:session_id])
-    unauthorized! unless @session.author == current_user && @session.pending_confirmation? && @session.review_decision && Time.zone.now <= ImportantDates::ACCEPTANCE_NOTIFICATION_DATETIME
+    unauthorized! unless @session.author == current_user && @session.pending_confirmation? && @session.review_decision && Time.zone.now <= ImportantDates::AUTHORS_INSCRIPTION_END_DATETIME
   end
 end

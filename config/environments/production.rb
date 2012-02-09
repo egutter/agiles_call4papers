466d3f942d8c52c80a0c81701ed4ced38747cc4f
Emilio Gutter <egutter@10pines.com>
2012-02-08 21:06:41 -0300

    Removed mailer delivery method for prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ AgilesCall4Papers::Application.configure do
   
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :ses
+  # config.action_mailer.delivery_method = :ses
 
   # Enable threaded mode
   # config.threadsafe!

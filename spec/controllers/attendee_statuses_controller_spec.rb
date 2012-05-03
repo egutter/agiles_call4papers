d7da6907a35a734ff477281fecebd128cfed2a36
Emilio Gutter <egutter@10pines.com>
2012-05-02 21:48:03 -0300

    trying to fix tests

--- spec/controllers/attendee_statuses_controller_spec.rb
+++ spec/controllers/attendee_statuses_controller_spec.rb
@@ -5,7 +5,7 @@ describe AttendeeStatusesController do
 
   describe "GET show" do
     it "should render show template" do
-      attendee = Factory(:attendee, :registration_date => Time.zone.local(2011, 4, 25))
+      attendee = Factory(:attendee, :registration_date => Time.zone.local(2012, 4, 25))
       get :show, :id => attendee.uri_token
       response.should render_template(:show)
       assigns(:attendee).should == attendee
